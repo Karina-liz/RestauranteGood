fa48982bbfb8511f5b77647fd0bf7bc9fe5f4701
Karina <u22220029@utp.edu.pe>
2024-11-08 11:17:03 -0500

    Commit inicial

---
+++ src/test/java/com/app/restaurante/RestauranteGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
