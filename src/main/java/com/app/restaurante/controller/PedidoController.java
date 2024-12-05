387e5e9c24e7fb538f465f1a80b34338530ca41e
Karina <u22220029@utp.edu.pe>
2024-12-05 01:08:18 -0500

    merge con admin

--- src/main/java/com/app/restaurante/controller/PedidoController.java
+++ src/main/java/com/app/restaurante/controller/PedidoController.java
@@ -1,15 +1,8 @@
 package com.app.restaurante.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.Model;
